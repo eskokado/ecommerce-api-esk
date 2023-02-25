597372d33e63e5d8f10d2df06cdc19a024d54075
eskokado <eskokado@gmail.com>
2023-02-25 15:26:39 -0300

    feat: spec Admin::V1::Categories as :client GET /catetories

---
+++ spec/requests/admin/v1/categories/client_requests_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Admin V1 Categories as :client", type: :request do
+  let(:user) { create(:user, profile: :client) }
+
+  context "GET /categories" do
+    let(:url) { "/admin/v1/categories" }
+    let!(:categories) { create_list(:category, 5) }
+    before(:each) { get url, headers: auth_header(user) }
+    include_examples "forbidden access"
+  end
+
+end
