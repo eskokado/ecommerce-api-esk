f5af990ca47cafb867d2f135d02f316fddc913db
eskokado <eskokado@gmail.com>
2023-02-27 07:18:26 -0300

    fix: remove method render_error and include SimpleErrorRendereble

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -5,19 +5,14 @@ module Admin::V1
     include Devise::Controllers::Helpers
     include Authenticable
 
+    include SimpleErrorRenderable
+    self.simple_error_partial = "shared/simple_error"
+
     rescue_from ForbiddenAccess do
       render_error(message: "Forbidden access", status: :forbidden)
     end
 
     before_action :restrict_access_for_admin!
-
-    def self.render_error(message: nil, fields: nil, status: :unprocessable_entity)
-      errors = {}
-      errors['fields'] = fields if fields.present?
-      errors['message'] = message if message.present?
-      result = { errors: errors }
-      render json: result, status: status
-    end
 
     private
 
