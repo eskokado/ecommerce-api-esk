0df5db0627a9e2968869faf180bcd2bce7bd3b48
eskokado <eskokado@gmail.com>
2023-02-27 06:42:41 -0300

    feat: config app with access restriction

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,7 +1,15 @@
 module Admin::V1
   class ApiController < ApplicationController
+    class ForbiddenAccess < StandardError; end
+
     include Devise::Controllers::Helpers
     include Authenticable
+
+    rescue_from ForbiddenAccess do
+      render_error(message: "Forbidden access", status: :forbidden)
+    end
+
+    before_action :restrict_access_for_admin!
 
     def self.render_error(message: nil, fields: nil, status: :unprocessable_entity)
       errors = {}
@@ -9,6 +17,12 @@ module Admin::V1
       errors['message'] = message if message.present?
       result = { errors: errors }
       render json: result, status: status
+    end
+
+    private
+
+    def restrict_access_for_admin!
+      raise ForbiddenAccess unless current_user.admin?
     end
   end
 end
