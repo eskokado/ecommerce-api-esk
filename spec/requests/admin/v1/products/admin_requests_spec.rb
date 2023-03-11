7cab4909182cbbc6eb0eae95ef9b74cd54ddbc03
eskokado <eskokado@gmail.com>
2023-03-11 14:45:50 -0300

    test: create get products returns success status

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe "Admin::V1::Products as :admin", type: :request do
       get url, headers: auth_header(user)
       expect(body_json['products']).to contain_exactly *products.as_json(only: %i(id name description price))
     end
+
+    it "returns success status" do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
 
