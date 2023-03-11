216f731b5b62229138ea579066feb54c18e14a18
eskokado <eskokado@gmail.com>
2023-03-11 14:44:02 -0300

    test: create get products returns all products

---
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::V1::Products as :admin", type: :request do
+  let(:user) { create(:user) }
+
+  context "GET /products" do
+    let(:url) { "/admin/v1/products" }
+    let!(:products) { create_list(:product, 5) }
+
+    it "returns all Products" do
+      get url, headers: auth_header(user)
+      expect(body_json['products']).to contain_exactly *products.as_json(only: %i(id name description price))
+    end
+  end
+end
+
