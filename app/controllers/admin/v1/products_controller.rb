7b768b08faba8cbed6337c6f493da8ce36d9a2cd
eskokado <eskokado@gmail.com>
2023-03-12 14:38:32 -0300

    feat: add method create into controller product

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -4,11 +4,25 @@ module Admin::V1
       @products = Product.all
     end
 
+    def create
+
+      @product = Product.new
+      @product.attributes = product_params
+      save_product!
+    end
+
     private
 
     def product_params
       return {} unless params.has_key?(:product)
-      params.require(:product).permit(:id, :name, :description, :price)
+      params.require(:product).permit(:id, :name, :description, :price, :image)
+    end
+
+    def save_product!
+      @product.save!
+      render json: @product, status: :created
+    rescue
+      render json: { errors: { fields: @product.errors.messages } }, status: :unprocessable_entity
     end
 
   end
