8600ba38824efc98f98845457cfcfdb6ea94480f
eskokado <eskokado@gmail.com>
2023-03-14 07:43:18 -0300

    test: DELETE /system_requirement/:id does not return any body content

--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -180,6 +180,11 @@ RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
         delete url, headers: auth_header(user)
         expect(response).to have_http_status(:no_content)
       end
+
+      it 'does not return any body content' do
+        delete url, headers: auth_header(user)
+        expect(body_json).to_not be_present
+      end
     end
   end
 end
