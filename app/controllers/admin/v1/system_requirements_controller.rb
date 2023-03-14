5c23a50c848ca82dc479ab227959874b808440e5
eskokado <eskokado@gmail.com>
2023-03-14 04:37:20 -0300

    feat: add action update in SystemRequirementsController

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -13,6 +13,15 @@ module Admin::V1
       render json: { errors: { fields: @system_requirement.errors.messages } }, status: :unprocessable_entity
     end
 
+    def update
+      @system_requirement = SystemRequirement.find(params[:id])
+      @system_requirement.attributes = system_requirement_params
+      @system_requirement.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @system_requirement.errors.messages } }, status: :unprocessable_entity
+    end
+
     private
 
     def system_requirement_params
