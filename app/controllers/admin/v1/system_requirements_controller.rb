16c59860c5cccd690f04449d307616b0baf6ef89
eskokado <eskokado@gmail.com>
2023-03-14 03:30:13 -0300

    feat: add action create in SystemController

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -3,5 +3,21 @@ module Admin::V1
     def index
       @system_requirements = SystemRequirement.all
     end
+
+    def create
+      @system_requirements = SystemRequirement.new
+      @system_requirements.attributes = system_requirements_params
+      @system_requirements.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @category.errors.messages } }, status: :unprocessable_entity
+    end
+
+    private
+
+    def system_requirements_params
+      return {} unless params.has_key?(:system_requirement)
+      params.require(:system_requirement).permit(:id, :name, :operational_system, :storage, :processor, :memory, :video_board)
+    end
   end
 end
