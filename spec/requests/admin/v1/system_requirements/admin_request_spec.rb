ec038b3ba5450ecd996226bbcdab88f2ed27463e
eskokado <eskokado@gmail.com>
2023-03-14 04:47:17 -0300

    test: PATCH /system_requirements/:id with valid params returns success status

--- spec/requests/admin/v1/system_requirements/admin_request_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -111,6 +111,11 @@ RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
         expected_system_requirement = SystemRequirement.last.to_json(only: %i(id name operational_system storage processor memory video_board))
         expect(response.body).to include_json(expected_system_requirement)
       end
+
+      it 'returns success status' do
+        patch url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
