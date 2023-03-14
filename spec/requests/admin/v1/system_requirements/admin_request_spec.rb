9b9ced11d825234ed9b77c4fd33f358ee20de117
eskokado <eskokado@gmail.com>
2023-03-14 03:07:11 -0300

    test: GET /system_requirements return all SystemRequirements

---
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require "rspec-json_matchers"
+
+RSpec.describe "Admin::V1::SystemRequirements as :admin", type: :request do
+  let(:user) { create(:user) }
+
+  context "GET /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+    let!(:system_requirements) { create_list(:system_requirement, 5) }
+
+    it "returns all SystemRequirements" do
+      get url, headers: auth_header(user)
+
+      expect(response.body).to include_json(system_requirements.to_json(only: %i(id name operational_system storage processor memory video_board)))
+    end
+  end
+end
