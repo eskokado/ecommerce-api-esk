6a1393ccb8ecf6ea06b6ecfe59a101853b14726d
eskokado <eskokado@gmail.com>
2023-03-15 12:12:38 -0300

    test: Admin V1 SystemRequirements POST /system_requirements returns unauthenticated access

--- spec/requests/admin/v1/system_requirements/unauthenticated_requests_spec.rb
+++ spec/requests/admin/v1/system_requirements/unauthenticated_requests_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe "Admin V1 SystemRequirements without authentication", type: :requ
     before(:each) { get url }
     include_examples "unauthenticated access"
   end
+
+  context "POST /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+    before(:each) { post url }
+    include_examples "unauthenticated access"
+  end
+
 end
