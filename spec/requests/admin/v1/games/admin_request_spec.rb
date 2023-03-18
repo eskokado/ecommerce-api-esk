85be3b67ce5184aa21bbb5e1972e822637d5e3c2
eskokado <eskokado@gmail.com>
2023-03-18 14:23:28 -0300

    test: PATCH /games/:id with invalid params returns unprocessable_entity status

--- spec/requests/admin/v1/games/admin_request_spec.rb
+++ spec/requests/admin/v1/games/admin_request_spec.rb
@@ -144,6 +144,11 @@ RSpec.describe "Admin::V1::Games as :admin", type: :request do
         expect(body['errors']['fields']).to have_key('system_requirement')
       end
 
+      it 'returns unprocessable_entity status' do
+        patch url, headers: auth_header(user), params: game_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
     end
   end
 end
