dbd7c534785376539dada47509c04ca207941ebf
eskokado <eskokado@gmail.com>
2023-03-18 10:01:02 -0300

    test: Admin::V1::Games as :admin GET /games returns all Games

---
+++ spec/requests/admin/v1/games/admin_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require "rspec-json_matchers"
+
+RSpec.describe "Admin::V1::Games as :admin", type: :request do
+  let(:user) { create(:user) }
+
+  context "GET /games" do
+    let(:url) { "/admin/v1/games" }
+    let!(:games) { create_list(:game, 5) }
+
+    it "returns all Games" do
+      get url, headers: auth_header(user)
+
+      expect(response.body).to include_json(games.to_json(only: %i(id mode release_date developer)))
+    end
+  end
+end
