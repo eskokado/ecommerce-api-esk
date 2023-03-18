6ca51c8433dea9765a3c8b2e2c1fec8da502db18
eskokado <eskokado@gmail.com>
2023-03-18 14:39:39 -0300

    test: DELETE /games/:id does not return any body content

--- spec/requests/admin/v1/games/admin_request_spec.rb
+++ spec/requests/admin/v1/games/admin_request_spec.rb
@@ -165,6 +165,11 @@ RSpec.describe "Admin::V1::Games as :admin", type: :request do
         delete url, headers: auth_header(user)
         expect(response).to have_http_status(:no_content)
       end
+
+      it 'does not return any body content' do
+        delete url, headers: auth_header(user)
+        expect(body_json).to_not be_present
+      end
     end
   end
 end
