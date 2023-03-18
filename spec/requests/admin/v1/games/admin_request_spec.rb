b2089d1fedeb2b3eba291d5d5ccb76491a949659
eskokado <eskokado@gmail.com>
2023-03-18 10:02:19 -0300

    test: Admin::V1::Games as :admin GET /games returns success status

--- spec/requests/admin/v1/games/admin_request_spec.rb
+++ spec/requests/admin/v1/games/admin_request_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe "Admin::V1::Games as :admin", type: :request do
 
       expect(response.body).to include_json(games.to_json(only: %i(id mode release_date developer)))
     end
+
+    it "returns success status" do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
