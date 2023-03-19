09f20b0063d260ec2d94f73da89adc2cb339e885
eskokado <eskokado@gmail.com>
2023-03-19 15:24:53 -0300

    test: Admin::V1::Coupons as :client GET /coupons return forbidden access

---
+++ spec/requests/admin/v1/coupons/client_requests_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Admin V1 Games as :client", type: :request do
+  let(:user) { create(:user, profile: :client) }
+
+  context "GET /coupons" do
+    let(:url) { "/admin/v1/coupons" }
+    let!(:coupons) { create_list(:coupon, 5) }
+    before(:each) { get url, headers: auth_header(user) }
+    include_examples "forbidden access"
+  end
+end
