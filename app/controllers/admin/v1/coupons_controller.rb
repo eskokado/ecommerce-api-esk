80b352cf24debcbb404523f74482cce6a29353ca
eskokado <eskokado@gmail.com>
2023-03-19 16:14:42 -0300

    feat: add action create coupon in CouponsController

--- app/controllers/admin/v1/coupons_controller.rb
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -3,5 +3,25 @@ module Admin::V1
     def index
       @coupons = Coupon.all
     end
+
+    def create
+      @coupon = Coupon.new
+      @coupon.attributes = coupon_params
+      save_coupon!
+    end
+
+    private
+
+    def coupon_params
+      return {} unless params.has_key?(:coupon)
+      params.require(:coupon).permit(:id, :name, :code, :status, :discount_value, :max_use, :due_date)
+    end
+
+    def save_coupon!
+      @coupon.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @game.errors.messages } }, status: :unprocessable_entity
+    end
   end
 end
