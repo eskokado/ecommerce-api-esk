b43a9e0a118205fdbe034dda78ac15545d671cd9
eskokado <eskokado@gmail.com>
2023-03-19 15:20:34 -0300

    test: Admin::V1::Coupons as :admin GET /coupons return all Coupons

---
+++ spec/requests/admin/v1/coupons/admin_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require "rspec-json_matchers"
+
+RSpec.describe "Admin::V1::Coupons as :admin", type: :request do
+  let(:user) { create(:user) }
+
+  context "GET /coupons" do
+    let(:url) { "/admin/v1/coupons" }
+    let!(:coupons) { create_list(:coupon, 5) }
+
+    it "returns all Coupons" do
+      get url, headers: auth_header(user)
+
+      expect(response.body).to include_json(coupons.to_json(only: %i(id name code status discount_value max_use due_date)))
+    end
+  end
+end
