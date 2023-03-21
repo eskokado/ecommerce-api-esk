cabf67156ab3f18d81d2d1a0eaecc99a414bc610
eskokado <eskokado@gmail.com>
2023-03-21 17:17:43 -0300

    test: create product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
+    association :productable, factory: :game
 
     after :build do |product|
-      product.productable = create(:game)
       product.categories << create(:category)
     end
   end
