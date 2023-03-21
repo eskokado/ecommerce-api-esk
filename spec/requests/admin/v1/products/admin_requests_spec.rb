96497798d02cc8d16939276f94d4cfd8a1f25022
eskokado <eskokado@gmail.com>
2023-03-21 17:30:10 -0300

    fix: POST /products with valid params returns success status

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -70,11 +70,12 @@ RSpec.describe "Admin::V1::Products as :admin", type: :request do
         )
         expect(response.body).to include_json(expected_product)
       end
+
+      it 'returns success status' do
+        post url, headers: auth_header(user), params: product_params
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'returns success status' do
-      post url, headers: auth_header(user), params: product_params
-      expect(response).to have_http_status(:ok)
-    end
   end
 end
