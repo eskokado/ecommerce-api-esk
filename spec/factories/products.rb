15788e3e37c3156353702a12beb906defc9d0e34
eskokado <eskokado@gmail.com>
2023-03-22 18:10:56 -0300

    fix: remove has_one_attached :image

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commerce.price(range: 100.0..400.0) }
-    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
+    price { Faker::Number.decimal(l_digits: 2, r_digits: 2) * Faker::Number.between(from: 100, to: 400) }
+    image { "product_image.png" }
     association :productable, factory: :game
 
     after :build do |product|
