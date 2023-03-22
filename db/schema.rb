c5175329091b04dc34bab621ef88c5bbf4f6ad90
eskokado <eskokado@gmail.com>
2023-03-22 18:10:11 -0300

    feat: add migration convert field image to string in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_19_150403) do
+ActiveRecord::Schema.define(version: 2023_03_22_193212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2023_02_19_150403) do
     t.bigint "productable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable_type_and_productable_id"
   end
 
