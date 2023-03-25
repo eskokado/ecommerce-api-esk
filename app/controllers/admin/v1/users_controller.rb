4639cf098918fc3f85b73c98b818801e96f088ea
eskokado <eskokado@gmail.com>
2023-03-25 07:42:33 -0300

    feat: add action create user in UsersController

--- app/controllers/admin/v1/users_controller.rb
+++ app/controllers/admin/v1/users_controller.rb
@@ -3,5 +3,25 @@ module Admin::V1
     def index
       @users = User.all
     end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+      save_user!
+    end
+
+    private
+
+    def user_params
+      return {} unless params.has_key?(:user)
+      params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :profile)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @user.errors.messages } }, status: :unprocessable_entity
+    end
   end
 end
