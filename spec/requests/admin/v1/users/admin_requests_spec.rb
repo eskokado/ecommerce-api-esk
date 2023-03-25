5e766b84b53569a139e0048c4ee32b0315913967
eskokado <eskokado@gmail.com>
2023-03-25 07:30:45 -0300

    test: Admin::V1::Users as :admin GET /users returns all Users

---
+++ spec/requests/admin/v1/users/admin_requests_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require "rspec-json_matchers"
+
+RSpec.describe "Admin::V1::Users as :admin", type: :request do
+  let(:user) { create(:user) }
+
+  context "GET /users" do
+    let(:url) { "/admin/v1/users" }
+    let!(:users) { create_list(:user, 5) }
+
+    it "returns all Users" do
+      get url, headers: auth_header(user)
+      users.append(user)
+      users.sort!{|a, b| a["id"]<=>b["id"]}
+      expect(response.body).to include_json(users.to_json(only: %i(id name email profile)))
+    end
+  end
+end
