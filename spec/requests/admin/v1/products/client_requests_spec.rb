2eb0f2a3098ccf76dfce797210fec6f28d5f22b4
eskokado <eskokado@gmail.com>
2023-03-25 03:46:36 -0300

    test: Admin V1 Products as :client POST /products returns forbidden access

--- spec/requests/admin/v1/products/client_requests_spec.rb
+++ spec/requests/admin/v1/products/client_requests_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe "Admin V1 Products as :client", type: :request do
     before(:each) { get url, headers: auth_header(user) }
     include_examples "forbidden access"
   end
+
+  context "POST /products" do
+    let(:url) { "/admin/v1/products" }
+    before(:each) { post url, headers: auth_header(user) }
+    include_examples "forbidden access"
+  end
 end
