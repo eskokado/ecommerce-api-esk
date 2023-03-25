636cfa99e034f2a2f1b3c46ff6a9be057d1b42d4
eskokado <eskokado@gmail.com>
2023-03-25 04:35:12 -0300

    test: move context PATCH /products/:id

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -112,90 +112,90 @@ RSpec.describe "Admin::V1::Products as :admin", type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
 
-    context "PATCH /products/:id" do
-      let(:product) { create(:product) }
-      let(:url) { "/admin/v1/products/#{product.id}" }
+  context "PATCH /products/:id" do
+    let(:product) { create(:product) }
+    let(:url) { "/admin/v1/products/#{product.id}" }
 
-      context "with valid params" do
-        let(:game) { create(:game) }
-        let(:new_name) { 'My new Product' }
-        let(:new_description) { 'My new Description Product' }
-        let(:new_price) { 150.0 }
-        let(:new_image) { 'new_image.png' }
-        let(:product_params) { { product: {
-          name: new_name,
-          description: new_description,
-          price: new_price,
-          image: new_image,
-          productable_id: game.id,
-          productable_type: "Game"
-        } }.to_json }
+    context "with valid params" do
+      let(:game) { create(:game) }
+      let(:new_name) { 'My new Product' }
+      let(:new_description) { 'My new Description Product' }
+      let(:new_price) { 150.0 }
+      let(:new_image) { 'new_image.png' }
+      let(:product_params) { { product: {
+        name: new_name,
+        description: new_description,
+        price: new_price,
+        image: new_image,
+        productable_id: game.id,
+        productable_type: "Game"
+      } }.to_json }
 
-        it 'updates Product' do
-          patch url, headers: auth_header(user), params: product_params
-          product.reload
-          expect(product.name).to eq new_name
-          expect(product.description).to eq new_description
-          expect(product.price).to eq new_price
-          expect(product.image).to eq new_image
-          expect(product.productable_id).to eq game.id
-        end
-
-        it 'returns updated Product' do
-          patch url, headers: auth_header(user), params: product_params
-          product.reload
-          expected_product = { product: Product.last.reload.as_json(
-            only: %i[id name description price image],
-            include: { productable: { only: %i[id mode release_date developer] }, categories: { only: %i[id name] } }
-          ) }.stringify_keys
-          expect(JSON.parse(response.body)).to eq(expected_product)
-        end
-
-        it 'returns success status' do
-          patch url, headers: auth_header(user), params: product_params
-          expect(response).to have_http_status(:ok)
-        end
+      it 'updates Product' do
+        patch url, headers: auth_header(user), params: product_params
+        product.reload
+        expect(product.name).to eq new_name
+        expect(product.description).to eq new_description
+        expect(product.price).to eq new_price
+        expect(product.image).to eq new_image
+        expect(product.productable_id).to eq game.id
       end
 
-      context "with invalid params" do
-        let(:product_invalid_params) { { product: {
-          name: nil,
-          description: nil,
-          price: nil,
-          image: nil,
-          productable_id: nil
-        } }.to_json }
+      it 'returns updated Product' do
+        patch url, headers: auth_header(user), params: product_params
+        product.reload
+        expected_product = { product: Product.last.reload.as_json(
+          only: %i[id name description price image],
+          include: { productable: { only: %i[id mode release_date developer] }, categories: { only: %i[id name] } }
+        ) }.stringify_keys
+        expect(JSON.parse(response.body)).to eq(expected_product)
+      end
 
-        it 'does not update Product' do
-          old_name = product.name
-          old_description = product.description
-          old_price = product.price
-          old_image = product.image
-          old_productable_id = product.productable_id
-          patch url, headers: auth_header(user), params: product_invalid_params
-          product.reload
-          expect(product.name).to eq old_name
-          expect(product.description).to eq old_description
-          expect(product.price).to eq old_price
-          expect(product.image).to eq old_image
-          expect(product.productable_id).to eq old_productable_id
-        end
+      it 'returns success status' do
+        patch url, headers: auth_header(user), params: product_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
 
-        it 'returns error message' do
-          patch url, headers: auth_header(user), params: product_invalid_params
-          body = JSON.parse(response.body)
-          expect(body['errors']['fields']).to have_key('name')
-          expect(body['errors']['fields']).to have_key('description')
-          expect(body['errors']['fields']).to have_key('price')
-          expect(body['errors']['fields']).to have_key('image')
-          expect(body['errors']['fields']).to have_key('productable')
-        end
+    context "with invalid params" do
+      let(:product_invalid_params) { { product: {
+        name: nil,
+        description: nil,
+        price: nil,
+        image: nil,
+        productable_id: nil
+      } }.to_json }
 
-        it 'returns unprocessable_entity status' do
-          patch url, headers: auth_header(user), params: product_invalid_params
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+      it 'does not update Product' do
+        old_name = product.name
+        old_description = product.description
+        old_price = product.price
+        old_image = product.image
+        old_productable_id = product.productable_id
+        patch url, headers: auth_header(user), params: product_invalid_params
+        product.reload
+        expect(product.name).to eq old_name
+        expect(product.description).to eq old_description
+        expect(product.price).to eq old_price
+        expect(product.image).to eq old_image
+        expect(product.productable_id).to eq old_productable_id
+      end
+
+      it 'returns error message' do
+        patch url, headers: auth_header(user), params: product_invalid_params
+        body = JSON.parse(response.body)
+        expect(body['errors']['fields']).to have_key('name')
+        expect(body['errors']['fields']).to have_key('description')
+        expect(body['errors']['fields']).to have_key('price')
+        expect(body['errors']['fields']).to have_key('image')
+        expect(body['errors']['fields']).to have_key('productable')
+      end
+
+      it 'returns unprocessable_entity status' do
+        patch url, headers: auth_header(user), params: product_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
