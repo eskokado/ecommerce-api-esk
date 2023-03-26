72231ccb005ca855c396b50e7b04357e58b09d4d
eskokado <eskokado@gmail.com>
2023-03-26 09:43:04 -0300

    test: Admin::V1::Categories as :admin GET /categories with pagination params returns records sized by :length

--- spec/requests/admin/v1/categories/admin_requests_spec.rb
+++ spec/requests/admin/v1/categories/admin_requests_spec.rb
@@ -48,6 +48,18 @@ RSpec.describe "Admin::V1::Categories as :admin", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    context "with pagination params" do
+      let(:page) { 2 }
+      let(:length) { 5 }
+
+      let(:pagination_params) { { page: page, length: length } }
+
+      it "returns records sized by :length" do
+        get url, headers: auth_header(user), params: pagination_params
+        expect(body_json['categories'].count).to eq length
+      end
+    end
   end
 
   context "POST /categories" do
