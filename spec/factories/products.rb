f69662ab185e2ce7b709cc0a131a36aafa1ca5cc
eskokado <eskokado@gmail.com>
2023-03-29 06:14:54 -0300

    fix: fix productable in Factory product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -5,7 +5,11 @@ FactoryBot.define do
     price { Faker::Number.decimal(l_digits: 2, r_digits: 2) * Faker::Number.between(from: 100, to: 400) }
     # image { "product_image.png" }
     status { :available }
-    association :productable, factory: :game
+    # association :productable, factory: :game
+
+    after :build do |product|
+      product.productable ||= create(:game)
+    end
 
     # after :build do |product|
     #   product.categories << create(:category)
