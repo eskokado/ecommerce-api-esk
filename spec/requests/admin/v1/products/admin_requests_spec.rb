74391385bab0e178693225d8c9869e559b2f4dec
eskokado <eskokado@gmail.com>
2023-03-31 13:02:30 -0300

    test: Admin V1 Products as :admin DELETE /products/:id removes productable

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -419,6 +419,12 @@ RSpec.describe "Admin V1 Products as :admin", type: :request do
         delete url, headers: auth_header(user)
       end.to change(Product, :count).by(-1)
     end
+
+    it 'removes productable' do
+      expect do
+        delete url, headers: auth_header(user)
+      end.to change(Game, :count).by(-1)
+    end
   end
 end
 
