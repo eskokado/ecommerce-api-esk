fbd8af2109299be64715fcc7597b7044bbcefeb3
eskokado <eskokado@gmail.com>
2023-03-31 11:26:35 -0300

    test: Admin V1 Products as :admin POST /products with invalid :productable params does not add a new productable

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -194,6 +194,12 @@ RSpec.describe "Admin V1 Products as :admin", type: :request do
           post url, headers: post_header, params: invalid_productable_params
         end.to_not change(Product, :count)
       end
+
+      it 'does not add a new productable' do
+        expect do
+          post url, headers: post_header, params: invalid_productable_params
+        end.to_not change(Game, :count)
+      end
     end
   end
 end
