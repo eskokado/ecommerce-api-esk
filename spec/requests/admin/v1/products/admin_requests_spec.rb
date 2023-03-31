3c6955fa3c736e54fdf0deb2ba5db870067a2376
eskokado <eskokado@gmail.com>
2023-03-31 11:23:55 -0300

    test: Admin V1 Products as :admin POST /products with invalid Product params returns unprocessable_entity status

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -176,6 +176,11 @@ RSpec.describe "Admin V1 Products as :admin", type: :request do
         post url, headers: post_header, params: product_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
+
+      it 'returns unprocessable_entity status' do
+        post url, headers: post_header, params: product_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
