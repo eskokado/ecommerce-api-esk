9b53a6a1920085515cb14e7e58efd29669a59987
eskokado <eskokado@gmail.com>
2023-04-01 07:54:03 -0300

    Revert "test: Admin::V1::Licenses as :admin GET /license without any params returns success status"
    This reverts commit 51dd235f79e1b6c2b681fc88eaade9adcf0b792e.

--- spec/requests/admin/v1/licenses/admin_requests_spec.rb
+++ spec/requests/admin/v1/licenses/admin_requests_spec.rb
@@ -8,22 +8,20 @@ RSpec.describe "Admin::V1::Licenses as :admin", type: :request do
     let(:url) { "/admin/v1/licenses" }
     let!(:licenses) { create_list(:license, 10) }
 
-    context "without any params" do
-      it "returns 10 Licenses" do
-        get url, headers: auth_header(user)
-        expect(body_json['licenses'].count).to eq 10
-      end
+    it "returns 10 Licenses" do
+      get url, headers: auth_header(user)
+      expect(body_json['licenses'].count).to eq 10
+    end
 
-      it "returns 10 first Licenses" do
-        get url, headers: auth_header(user)
-        expected_licenses = licenses[0..9].as_json(only: %i(id key game_id user_id))
-        expect(body_json['licenses']).to match_array expected_licenses
-      end
+    it "returns 10 first Licenses" do
+      get url, headers: auth_header(user)
+      expected_licenses = licenses[0..9].as_json(only: %i(id key game_id user_id))
+      expect(body_json['licenses']).to match_array expected_licenses
+    end
 
-      it "returns success status" do
-        get url, headers: auth_header(user)
-        expect(response).to have_http_status(:ok)
-      end
+    it "returns success status" do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
     end
   end
 
