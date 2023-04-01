c5c4374a8bd44bfcc4170a8850418e227885d89b
eskokado <eskokado@gmail.com>
2023-04-01 07:05:11 -0300

    test: has_many licenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:profile) }
   it { is_expected.to define_enum_for(:profile).with_values({ admin: 0, client: 1 }) }
 
+  it { is_expected.to have_many :licenses }
+
   it_behaves_like "name searchable concern", :user
   it_behaves_like "paginatable concern", :user
 end
