50564097fc5a242d74374d28e6e9ee387ad3da9c
eskokado <eskokado@gmail.com>
2023-04-01 09:23:52 -0300

    feat: create action create into LicensesController

--- app/controllers/admin/v1/licenses_controller.rb
+++ app/controllers/admin/v1/licenses_controller.rb
@@ -3,5 +3,24 @@ module Admin::V1
     def index
       @licenses = License.all
     end
+
+    def create
+      @license = License.new
+      @license.attributes = license_params
+      save_license!
+    end
+
+    private
+    def license_params
+      return {} unless params.has_key?(:license)
+      params.require(:license).permit(:id, :key, :game_id, :user_id)
+    end
+
+    def save_license!
+      @license.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @license.errors.messages } }, status: :unprocessable_entity
+    end
   end
 end
