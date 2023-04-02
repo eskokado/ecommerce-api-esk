4eca7674c87b726d60e87b4d7ba9982485c5c994
eskokado <eskokado@gmail.com>
2023-04-02 05:48:04 -0300

    test: Admin::ModelLoadingService when #call when params are present performs right :length following pagination

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -15,34 +15,14 @@ describe Admin::ModelLoadingService do
         { search: { name: "Search" }, order: { name: :desc }, page: 2, length: 4 }
       end
 
-      it "returns right :length following pagination" do
+      it "performs right :length following pagination" do
         service = described_class.new(Category.all, params)
-        result_categories = service.call
-        expect(result_categories.count).to eq 4
-      end
-
-      it "returns records following search, order and pagination" do
-        search_categories.sort! { |a, b| b[:name] <=> a[:name] }
-        service = described_class.new(Category.all, params)
-        result_categories = service.call
-        expected_categories = search_categories[4..7]
-        expect(result_categories).to contain_exactly *expected_categories
+        service.call
+        expect(service.records.count).to eq 4
       end
     end
 
     context "when params are not present" do
-      it "returns default :length pagination" do
-        service = described_class.new(Category.all, nil)
-        result_categories = service.call
-        expect(result_categories.count).to eq 10
-      end
-
-      it "returns first 10 records" do
-        service = described_class.new(Category.all, nil)
-        result_categories = service.call
-        expected_categories = categories[0..9]
-        expect(result_categories).to contain_exactly *expected_categories
-      end
     end
   end
 end
