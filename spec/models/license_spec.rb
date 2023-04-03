7aa658496fcdf4d1f6c6a02bbbcceeb0c0bf0836
eskokado <eskokado@gmail.com>
2023-04-03 09:11:02 -0300

    test: implement like searchable into models

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe License, type: :model do
   it { is_expected.to define_enum_for(:status).with_values({ available: 1, in_use: 2, inative: 3 })}
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :game }
+
+  it_behaves_like "paginatable concern", :license
+  it_has_behavior_of "like searchable concern", :license, :key
 end
