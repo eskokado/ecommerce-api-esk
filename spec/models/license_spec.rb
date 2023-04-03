f668ac1a1458cd469caeffbc56a8ddc1a51f7993
eskokado <eskokado@gmail.com>
2023-04-03 07:46:37 -0300

    test: add fields plataform and status into license test

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe License, type: :model do
   it { is_expected.to validate_presence_of(:key) }
+  it { is_expected.to validate_presence_of(:platform) }
+  it { is_expected.to define_enum_for(:platform).with_values({ steam: 1, battle_net: 2, origin: 3 })}
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to define_enum_for(:status).with_values({ available: 1, in_use: 2, inative: 3 })}
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :game }
 end
