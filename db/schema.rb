4dac3ee23c2154498d00d4ea561f56d8676c8df5
eskokado <eskokado@gmail.com>
2023-04-03 07:44:35 -0300

    feat: create migration alter license schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_01_094725) do
+ActiveRecord::Schema.define(version: 2023_04_03_090323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,11 +65,13 @@ ActiveRecord::Schema.define(version: 2023_04_01_094725) do
   end
 
   create_table "licenses", force: :cascade do |t|
-    t.integer "key"
+    t.string "key"
     t.bigint "user_id", null: false
     t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "platform"
+    t.integer "status"
     t.index ["game_id"], name: "index_licenses_on_game_id"
     t.index ["user_id"], name: "index_licenses_on_user_id"
   end
