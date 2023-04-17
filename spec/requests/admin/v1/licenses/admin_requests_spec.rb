3944ae7a5519cf0df67527627139a906185eafb4
eskokado <eskokado@gmail.com>
2023-04-17 10:59:23 -0300

    test: GET /games/:game_id/licenses with order params pagination meta attributes

--- spec/requests/admin/v1/licenses/admin_requests_spec.rb
+++ spec/requests/admin/v1/licenses/admin_requests_spec.rb
@@ -98,6 +98,10 @@ RSpec.describe "Admin::V1::Licenses as :admin", type: :request do
         get url, headers: auth_header(user), params: order_params
         expect(response).to have_http_status(:ok)
       end
+
+      it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total: 10, total_pages: 1 } do
+        before { get url, headers: auth_header(user), params: order_params }
+      end
     end
   end
 
