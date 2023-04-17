2d8342375b197285631b7e19c764ebea15ff73df
eskokado <eskokado@gmail.com>
2023-04-17 10:45:42 -0300

    test: GET /games/:game_id/licenses with pagination params returns success status

--- spec/requests/admin/v1/licenses/admin_requests_spec.rb
+++ spec/requests/admin/v1/licenses/admin_requests_spec.rb
@@ -73,6 +73,11 @@ RSpec.describe "Admin::V1::Licenses as :admin", type: :request do
         expected_licenses = licenses[5..9].as_json(only: %i(id key platform status game_id user_id))
         expect(body_json['licenses']).to match_array expected_licenses
       end
+
+      it "returns success status" do
+        get url, headers: auth_header(user), params: pagination_params
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
